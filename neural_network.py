ea8e21accd43358806c754930ed4c9259c45ec52
dtomassi <datomassi@ucdavis.edu>
2019-11-05 15:28:17 -0800

    Adding functionality for preprocessing data

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,6 @@
+import codecs
 import numpy as np
+import re
 import time
 
 from datetime import datetime
@@ -18,3 +20,26 @@ def train_model(training_data, training_labels, evaluation_data, evaluation_labe
     print('Accuracy: %.2f' % (accuracy*100))
 
     model.save('model-{}.h5'.format(int(time.time())))
+
+def preprocess_data(genuine_filepath, bot_filepath):
+    # Open csv file and get the tweet part of the csv.
+    # Strip out newlines and quotes around text.
+    with codecs.open(bot_filepath, 'r', encoding='utf-8', errors='ignore') as bots_file:
+        bot_sentences = [x.split(',')[1].strip('\n').strip('"').lower()
+                     if len(x.split(',')) > 1 else '' for x in bots_file.readlines()]
+    bot_sentences = bot_sentences[1:]
+    
+    with codecs.open(genuine_filepath, 'r', encoding='utf-8', errors='ignore') as genuine_file:
+        genuine_sentences = [x.split(',')[1].strip('\n').strip('"').lower()
+                     if len(x.split(',')) > 1 else '' for x in genuine_file.readlines()]
+    genuine_sentences = genuine_sentences[1:]
+
+    # Make space between # and hashtag
+    bot_sentences.replace('#', '# ')
+    genuine_sentences.replace('#', '# ')
+
+    # Split sentences into tokens
+    bot_sentences = [sentence.split(' ') for sentence in bot_sentences]
+    genuine_sentences = [sentence.split(' ') for sentence in genuine_sentences]
+
+    return bot_sentences, genuine_sentences
