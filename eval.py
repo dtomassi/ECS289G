32fa4a8e1036564356d0bafd3acde5fc77eef470
dtomassi <datomassi@ucdavis.edu>
2019-11-13 11:40:52 -0800

    Adding changes to eval

--- eval.py
+++ eval.py
@@ -1,46 +1,44 @@
+import ast
 import codecs
 
 from neural_network import train_model
+from neural_network import segment_data
 from word2vec import create_training_data
 from gensim.models import Word2Vec
 
-bots_fp = '/home/dtomassi/tweets_dataset/datasets_full.csv/social_spambots_2.csv/tweets.csv'
-genuine_fp = '/home/dtomassi/tweets_dataset/datasets_full.csv/genuine_accounts.csv/tweets.csv'
-eval_fp = '/home/dtomassi/tweets_dataset/datasets_full.csv/traditional_spambots_1.csv/tweets.csv'
+LENGTH_TWEETS = 20
+bots_fp = '/home/dtomassi/github/ECS289G/bot_tokenized_sentences.txt'
+gen_fp = '/home/dtomassi/github/ECS289G/gen_tokenized_sentences.txt'
 
-with codecs.open(bots_fp, 'r', encoding='utf-8', errors='ignore') as bots_file:
-    bot_sentences = [x.split(',')[1].strip('\n').strip('"').lower()
-                 if len(x.split(',')) > 1 else '' for x in bots_file.readlines()]
-bot_sentences = bot_sentences[1:]
-with codecs.open(genuine_fp, 'r', encoding='utf-8', errors='ignore') as genuine_file:
-    genuine_sentences = [x.split(',')[1].strip('\n').strip('"').lower()
-                 if len(x.split(',')) > 1 else '' for x in genuine_file.readlines()]
-genuine_sentences = genuine_sentences[1:]
-with codecs.open(eval_fp, 'r', encoding='utf-8', errors='ignore') as eval_file:
-    eval_sentences = [x.split(',')[1].strip('\n').strip('"').lower()
-                 if len(x.split(',')) > 1 else '' for x in eval_file.readlines()]
-eval_sentences = genuine_sentences[1:]
-print('Read in files')
+with open(bots_fp) as bots_file:
+    pos_tokenizes_sentences = [ast.literal_eval(x.strip()) for x in bots_file.readlines()]
 
-pos_tokenizes_sentences = [sentence.split(' ') for sentence in bot_sentences][:400000]
-neg_tokenizes_sentences = [sentence.split(' ') for sentence in genuine_sentences][:600000]
-eval_tokenizes_sentences = [sentence.split(' ') for sentence in eval_sentences][:600000]
-print('Tokenized sentences')
+with open(gen_fp) as gen_file:
+    neg_tokenizes_sentences = [ast.literal_eval(x.strip()) for x in gen_file.readlines()]
 
-neg_tokenizes_sentences_10 = list(filter(lambda x: len(x) <= 10, neg_tokenizes_sentences))
-pos_tokenizes_sentences_10 = list(filter(lambda x: len(x) <= 10, pos_tokenizes_sentences))
-eval_tokenizes_sentences_10 = list(filter(lambda x: len(x) <= 10, eval_tokenizes_sentences))
+pruned_pos_tokenizes_sentences = []
+for sentence in pos_tokenizes_sentences:
+    if sentence != []:
+        pruned_pos_tokenizes_sentences.append(sentence)
+
+pruned_neg_tokenizes_sentences = []
+for sentence in neg_tokenizes_sentences:
+    if sentence != []:
+        pruned_neg_tokenizes_sentences.append(sentence)
+
+neg_tokenizes_sentences_10 = list(filter(lambda x: len(x) <= LENGTH_TWEETS, pruned_neg_tokenizes_sentences))
+pos_tokenizes_sentences_10 = list(filter(lambda x: len(x) <= LENGTH_TWEETS, pruned_pos_tokenizes_sentences))
 print('Filtered tweets longer than 10 words')
 
-word2vec_model = Word2Vec.load("word2vec.model")
-neg_training_data, neg_training_labels = create_training_data(neg_tokenizes_sentences_10, word2vec_model, 0, 10)
-pos_training_data, pos_training_labels = create_training_data(pos_tokenizes_sentences_10, word2vec_model, 1, 10)
-eval_training_data, eval_training_labels = create_training_data(eval_tokenizes_sentences_10, word2vec_model, 1, 10)
+word2vec_model = Word2Vec.load("word2vec-1573008850.model")
+neg_data, neg_labels = create_training_data(neg_tokenizes_sentences_10, word2vec_model, 0, LENGTH_TWEETS)
+pos_data, pos_labels = create_training_data(pos_tokenizes_sentences_10, word2vec_model, 1, LENGTH_TWEETS)
 print('Generated training and evaluation data')
 
-training_data = [*pos_training_data, *neg_training_data]
-training_labels = [*pos_training_labels, *neg_training_labels]
-
+training_tuple, eval_tuple = segment_data(pos_data, neg_data)
+training_tuple_sep, eval_tuple_sep = list(map(list, zip(*training_tuple))), list(map(list, zip(*eval_tuple)))
+training_data, training_labels = training_tuple_sep[0], training_tuple_sep[1]
+eval_training_data, eval_training_labels = eval_tuple_sep[0], eval_tuple_sep[1]
 print('Training Model')
-train_model(training_data, training_labels, eval_training_data, eval_training_labels)
+train_model(training_data[:int(len(training_data)/4)], training_labels[:int(len(training_labels)/4)], eval_training_data, eval_training_labels)
 print('Finished training model')
